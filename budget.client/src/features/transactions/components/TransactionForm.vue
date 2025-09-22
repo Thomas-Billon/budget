c3518ec978bd74dae5a9c687cad402a28ba90264
Thomas <thomas@widilo.com>
2025-09-22 16:19:58 +0200

    Fixed patch update on select fields

--- budget.client/src/features/transactions/components/TransactionForm.vue
+++ budget.client/src/features/transactions/components/TransactionForm.vue
@@ -75,7 +75,12 @@
 
         // Edit only for partial update
         if (!isNew) {
-            partialModel[fieldName] = model.value[fieldName];
+            const target = event.target as HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement | null;
+            if (target === null) {
+                return;
+            }
+
+            partialModel[fieldName] = target.value as ITransactionRequest[T];
             debounceSavePartial();
         }
     }
