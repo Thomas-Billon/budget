b403b4c5152121152131d2f7b0e7962a88e873f2
Thomas <thomas@widilo.com>
2025-09-30 01:26:23 +0200

    Added decimal migration

--- Budget.Server/Data/Migrations/20250929232402_SwitchTransactionAmountToDecimal.Designer.cs
+++ Budget.Server/Data/Migrations/20250929232402_SwitchTransactionAmountToDecimal.Designer.cs
@@ -4,6 +4,7 @@ using Budget.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Budget.Server.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250929232402_SwitchTransactionAmountToDecimal")]
+    partial class SwitchTransactionAmountToDecimal
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
