c3df59fbe145ed98d078358a1316c3e86cbc36ef
Thomas <thomas@widilo.com>
2025-10-01 23:02:31 +0200

    Added date filter on balance details

--- budget.client/src/features/balances/views/DetailsView.vue
+++ budget.client/src/features/balances/views/DetailsView.vue
@@ -2,17 +2,23 @@
 
     import './DetailsView.scss';
 
-    import { onMounted, ref } from 'vue';
+    import { onMounted, ref, watch } from 'vue';
     import { apiCall } from '@/utils/ApiCall.ts';
     import { type IBalanceDetailsResponse } from '@/features/balances/models/IBalanceDetailsResponse.ts';
     import { getDatesFromDateRange } from '@/utils/DateRange.ts';
     import { DateRange } from '@/enums/DateRange.ts';
 
+    const dateRange = ref<DateRange>(DateRange.CurrentMonth);
     const balanceDetails = ref<IBalanceDetailsResponse | undefined>();
 
     // Init
     onMounted(() => {
         const { startDate, endDate } = getDatesFromDateRange(DateRange.CurrentMonth);
+        getBalanceDetails(startDate, endDate);
+    });
+
+    watch(dateRange, (newDateRange) => {
+        const { startDate, endDate } = getDatesFromDateRange(newDateRange);
         getBalanceDetails(startDate, endDate);
     });
 
@@ -33,6 +39,18 @@
 
 <template>
     <div class="balance-details section-container container">
+        <select class="form-select form-select-lg" id="balance-date-range" name="DateRange" v-model="dateRange">
+            <option :value="DateRange.Today">Today</option>
+            <option :value="DateRange.Yesterday">Yesterday</option>
+            <option :value="DateRange.CurrentWeek">Current week</option>
+            <option :value="DateRange.LastWeek">Last week</option>
+            <option :value="DateRange.CurrentMonth">Current month</option>
+            <option :value="DateRange.LastMonth">Last month</option>
+            <option :value="DateRange.CurrentYear">Current year</option>
+            <option :value="DateRange.LastYear">Last year</option>
+            <option :value="DateRange.AllTime">All time</option>
+        </select>
+
         <div class="balance-summary">
             <p>Total Income: {{ balanceDetails?.totalIncome }}</p>
             <p>Total Expense: {{ balanceDetails?.totalExpense }}</p>
