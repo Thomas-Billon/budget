db15f323cfa50119c2e41f77849f4ee9176bcb31
Thomas <thomas@widilo.com>
2026-07-31 19:51:14 +0200

    Added hello world endpoint

---
+++ Budget.Server/Api/Tests/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Budget.Server.Api.Tests
+{
+    [Route("[controller]")]
+    public class TestController : ApiControllerBase
+    {
+        [HttpGet("hello-world")]
+        [AllowAnonymous]
+        public async Task<IActionResult> HelloWorld()
+        {
+            return Ok(new { Result = "Hello, world!" });
+        }
+    }
+}
